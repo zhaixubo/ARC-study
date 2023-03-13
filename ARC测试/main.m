984f6c5b612bd6947147ca1231fad8012fa168dd
zhaixubo <113840032+zhaixubo@users.noreply.github.com>
2023-03-13 16:43:54 +0800

    Initial Commit

---
+++ ARC测试/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ARC测试
+//
+//  Created by 翟旭博 on 2023/3/13.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
